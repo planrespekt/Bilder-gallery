e89b893bdf046090d28e3f49a455fbed42ff2036
planrespekt <planrespekt@gmail.com>
2023-11-22 21:25:29 +0100

    Neon light

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+function darkLightMode(){
+    let toggleButton = document.querySelector("#toggleButton");
+    console.log(toggleButton);
+    let body = document.querySelector("body");
+    console.log(body);
+    
+    body.classList.toggle("dark")
+}
+
+toggleButton.addEventListener("click",darkLightMode)
